2c68fb205c22885f26b8b5c7893487fead87adea
mrChernicharo <felipe.chernicharo@gmail.com>
2020-05-31 19:30:19 -0300

    origin

---
+++ origin.py
@@ -0,0 +1,29 @@
+from time import sleep 
+from render_forca import render_forca
+
+
+game_loop = True
+erros = 0
+
+
+
+while game_loop:
+    render_forca(erros)
+    erros += 1
+    input('aperte enter')
+    
+    # game over
+    while erros > 6:
+        game_over = input('deseja jogar novamente? [s/n] ')    
+
+        if game_over == 's':
+            game_loop = True
+            erros = 0
+            break
+        elif game_over == 'n':
+            game_loop = False
+            break
+        else:    
+            print ('opçao invalida.')    
+
+
